0a936e4ce5bec36df99b3bce4e5d33decc73a921
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-05-27 17:21:47 -0300

    feat: implement ask_instruction

--- src/main.py
+++ src/main.py
@@ -1,35 +1,72 @@
-# AIzaSyAPtaTERZKDaUb6gGwl1UWBOCK_qvBRGm0
 import json
-import subprocess
 import questionary
-from common import GhDryRunParser
-from config import ConfigManager
-from config.config import GEMINI_KEY_PROPERTY
+from config import config_manager
+from config.config import GEMINI_KEY_PROPERTY, INSTRUCTIONS_PROPERTY
+from gemini.PrPrompt import PrPrompt
+from gemini.PromptManager import PromptManager
+from gh import GhManager
+from gh.PullRequest import PullRequest
 
-config_manager = ConfigManager("teste")
+
+ai_manager = PromptManager()
+
+
+# "reescreva o body do pr em portugues, mantenha o tipo do commit seguindo conventional commits, nao traduza o tipo do commit"
+
+
+def ask_create_instruction():
+  instruction = questionary.text("Write instructions for the AI").ask()
+  saved_instructions = config_manager.get(INSTRUCTIONS_PROPERTY) or []
+
+  saved_instructions.append(instruction)
+  config_manager.save(INSTRUCTIONS_PROPERTY, saved_instructions)
+  return saved_instructions
+
+
+def ask_ai_instruction():
+  instructions: list[str] = config_manager.get(INSTRUCTIONS_PROPERTY)
+  if not instructions:
+    instructions = ask_create_instruction()
+
+  choices: list[questionary.Choice] = []
+  choices.append(questionary.Choice("Create new prompt", "new_prompt"))
+
+  for instruction in instructions:
+    choices.append(questionary.Choice(instruction, instruction))
+
+  selected_instruction = questionary.select(
+    "Choose the AI prompt", choices=["Create new prompt"] + instructions
+  ).ask()
+
+  if selected_instruction == "new_prompt":
+    ask_create_instruction()
+    ask_ai_instruction()
+
+  return selected_instruction
+
+
+def ensure_ai_key():
+  if not config_manager.get(GEMINI_KEY_PROPERTY):
+    key = questionary.text("What's your Gemini AI key").ask()
+    config_manager.save(GEMINI_KEY_PROPERTY, key)
+
+
+def create_pull_request(instruction : str):
+  pr_json = GhManager.create_dry_pr()
+  raw_new_pr_json = ai_manager.prompt_pr(PrPrompt(pr_json, instruction))
+  new_pr = PullRequest(**json.loads(raw_new_pr_json))
+  return GhManager.create_pr(new_pr)
 
 
 def main():
-  if not config_manager.get(GEMINI_KEY_PROPERTY):
-    prompt_ai_key()
+  ensure_ai_key()
+  instruction = ask_ai_instruction()
+
+  gh_response = create_pull_request(instruction)
+
+  print(gh_response)
+  print("Succesfully created pull request")
 
 
-def prompt_ai_statement():
-  pass
-
-
-def prompt_ai_key():
-  key = questionary.text("What's your Gemini AI key").ask()
-  config_manager.save(GEMINI_KEY_PROPERTY, key)
-
-
-def get_pr_json():
-  command_output = (
-    subprocess.Popen("gh pr create --dry-run -f", shell=True, stdout=subprocess.PIPE)
-    .stdout.read()  # type: ignore
-    .decode()
-  ).split("\n", 1)[1]
-  parser = GhDryRunParser(command_output)
-  return parser.parse_output()
-
-print(json.dumps(get_pr_json()))
+if __name__ == "__main__":
+  main()
